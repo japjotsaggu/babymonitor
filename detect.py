d88f5a123304054ca44288851ab5233ae98cdff2
Japjot <119132799+japjotsaggu@users.noreply.github.com>
2024-04-03 13:30:11 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,11 +2,12 @@ import torch
 import cv2
 from torchvision.transforms.functional import InterpolationMode
 from yolov5.models.experimental import attempt_load
-from yolov5.utils.general import non_max_suppression, scale_coords
+from yolov5.utils.general import non_max_suppression
 from yolov5.utils.datasets import letterbox
 
 def detect_objects(video_path):
-    weights = 'yolov5x.pt'  
+
+    weights = 'yolov5x.pt' 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = attempt_load(weights, map_location=device)
 
@@ -17,7 +18,7 @@ def detect_objects(video_path):
             break
 
         img = letterbox(frame, new_shape=640)[0]
-        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, HWC to CHW
+        img = img[:, :, ::-1].transpose(2, 0, 1)  
         img = torch.from_numpy(img).float().to(device) / 255.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
@@ -25,10 +26,8 @@ def detect_objects(video_path):
         pred = model(img)[0]
         pred = non_max_suppression(pred, 0.4, 0.5)
 
-        # Process detections
         for det in pred:
             if det is not None and len(det):
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], frame.shape).round()
                 for *xyxy, conf, cls in reversed(det):
                     label = f'{model.names[int(cls)]} {conf:.2f}'
                     cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (255, 0, 0), 2)
